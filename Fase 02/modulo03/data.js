bd6fdafc8bcc5abbd9959a6138569b7d89efd37d
Rafadcosta <rafaeladuqu@gmail.com>
2020-04-26 20:08:32 -0300

    usando dados dinâmicos

---
+++ Fase 02/modulo03/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "BvhYm0BOLvA",
+        title: "Dark Mode raiz com CSS e JavaScript | Code/Drops #26",
+        duration: "17:35 min",
+        price: "Free"
+    },
+    {
+        id: "DiXbJL3iWVs",
+        title: "Node.js: Iniciando da teoria à prática | Masterclass #11",
+        duration: "1:33:18 min",
+        price: "Free"
+    },
+    {
+        id: "OOecQMZMkqY",
+        title: "Como Publicar Pacotes no NPM | Code/Drops #24",
+        duration: "14:40 min",
+        price: "Free"
+    },
+    {
+        id: "nDN4JRbFEns",
+        title: "NestJS + TypeORM + GraphQL | Code Challenge",
+        duration: "3:11:06 min",
+        price: "Free"
+    },
+    {
+        id: "nhW70H9H4gU",
+        title: "Espaçamentos e a mágica do CSS Box Model | Masterclass #10",
+        duration: "39:12 min",
+        price: "Free"
+    },
+    {
+        id: "dlCUvYOlnDo",
+        title: "Usando Serverless, Jest e Node.js para corrigir desafios | Behind the Code #13",
+        duration: "55:51 min",
+        price: "Free"
+    }
+]
--- Fase 02/modulo03/server.js
+++ Fase 02/modulo03/server.js
@@ -2,21 +2,46 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require("./data");
 
 server.use(express.static('public'));
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 });
 
 server.get("/", function(req, res) {
-    return res.render("about"); 
+
+    const about = {
+        avatar_url: "https://avatars1.githubusercontent.com/u/47401318?s=460&u=82265c6f81b2cead0be40b8eb0daebdecf2d9897&v=4",
+        name: "Rafaela Duque",
+        role: "Estudante - Rocketseat",
+        description: '20 anos, <a href="https://www.linkedin.com/in/rafaela-duque/" target="_blank">Analista de Suporte</a>.  Visando mudança de carreira para programação web front-end.',
+        link: [
+            {
+                name: "Github",
+                url: "https://github.com/Rafadcosta"
+            },
+            {
+                name: "Twitter",
+                url: "https://twitter.com/rafaduquec_"
+            },
+            {
+                name: "LinkedIn",
+                url: "https://www.linkedin.com/in/rafaela-duque/"
+            }
+        ]
+    }
+
+    return res.render("about", {about: about}); 
 });
 
 server.get("/portfolio", function(req, res) {
-    return res.render("portfolio");
-});
+
+    return res.render("portfolio", {items: videos});
+}); 
 
 server.listen(5000, function() {
     console.log("Server is running");
