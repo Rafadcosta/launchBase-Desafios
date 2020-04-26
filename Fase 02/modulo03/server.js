f675b721383a53b1aaeeaf6e17182e3d49509175
Rafadcosta <rafaeladuqu@gmail.com>
2020-04-26 19:29:08 -0300

    template engine

--- Fase 02/modulo03/server.js
+++ Fase 02/modulo03/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
-server.get("/", function(req, res) {
-    return res.send('Hi my sweet darling aushauh');
+server.use(express.static('public'));
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server
 });
 
+server.get("/", function(req, res) {
+    return res.render("about"); 
+});
+
+server.get("/portfolio", function(req, res) {
+    return res.render("portfolio");
+});
 
 server.listen(5000, function() {
-    console.log('Server is running');
+    console.log("Server is running");
 });
